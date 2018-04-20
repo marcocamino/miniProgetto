4b12a63b84a2a2bebad09ac587cf486c21d7bbe6
marcocamino <marco1.camino@gmail.com>
2018-04-20 22:36:32 +0200

    ok

--- src/miniProgetto/Test.java
+++ src/miniProgetto/Test.java
@@ -5,6 +5,11 @@ import java.util.List;
 public class Test {
 	public static void main(String[] args){
 		
+		//usr marcocamino
+		
+		//email marco1.camino@gmail.com
+		
+		//pwd m4rc0C16!
 		System.out.println("salve mondo");
 		
 
